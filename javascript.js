6a67211b87802d1e6c8f0eba02b915ce5cbe8a6a
Stephen Smith <stephensmith750@gmail.com>
2022-07-31 22:17:06 +0100

    Complete calculator project
    - Add keydown event handlers
    - Add shake animation on error
    - Include power function

--- javascript.js
+++ javascript.js
@@ -1,8 +1,11 @@
+//Global variables
 let x = "";
 let y = "";
 let operation = "";
 let result;
 
+
+//Arithmetic functions
 function add(a, b) {
     return a + b;
 }
@@ -19,6 +22,11 @@ function divide(a, b) {
     return a / b;
 }
 
+function exp(a, b) {
+   return Math.pow(a, b);
+
+}
+
 function operate(a, b, operator) {
     if (operator === '+') {
         return add(a, b);
@@ -27,11 +35,17 @@ function operate(a, b, operator) {
     } else if (operator === '*') {
         return multiply(a, b);
     } else if (operator === '/') {
-        if (b === 0) return 'DIV 0 ERR';
+        if (b === 0) {
+            return 'DIV0';
+        }
         return divide(a, b);
+    } else if (operator === 'x<sup>y</sup>') {
+        return exp(a, b);
     }
 }
 
+
+//Resets the calculator. A hard reset wipes the current running total.
 function reset(hardReset) {
     updateScreen(0);
     x = "";
@@ -42,6 +56,7 @@ function reset(hardReset) {
     }
 }
 
+//Evaluates the result of the current operation.
 function evaluate() {
     if (operation !== "" && y !== "") {
         result = operate(Number(x), Number(y), operation);
@@ -57,25 +72,55 @@ function evaluate() {
     } 
 }
 
+function appendX(str) {
+    x = x + str;
+}
 
-function executeButton(e) {  
+function appendY(str) {
+    y = y + str;
+}
+
+function shake() {
+    const container = document.querySelector('.calculator');
+    console.log(container);
+    container.classList.add('shake');
+    container.addEventListener('animationend', function() {
+        this.classList.remove('shake')
+    });
+}
+
+//updates the screen with the new value, reduces the font size to fit the screen size as the number gets bigger.
+function updateScreen(value) {
+    const scrn = document.querySelector('.screen');
+    scrn.style.fontSize = '72px';
+    scrn.innerHTML = value;   
+    fontSize = parseFloat(window.getComputedStyle(scrn, null).fontSize);
+    while (scrn.scrollWidth > scrn.clientWidth) {
+        fontSize--;
+        scrn.style.fontSize = `${fontSize}px`;
+    }
+ 
+}
+
+//button click and key press functions.
+function clickButton(e) {  
     if (this.innerHTML === 'AC') {
         reset(true);
     } else if (this.innerHTML === '=') {
         evaluate();        
     } else if (this.classList.contains('number')) {
         if ((!result) && operation === "") {
-            x = x + this.innerHTML;
+            appendX(this.innerHTML);
             updateScreen(x);
         } else if (operation !== "") {
-            y = y + this.innerHTML;
+            appendY(this.innerHTML);
             updateScreen(y);
         } else if (result === x) {
             reset();
-            x = x + this.innerHTML;
+            appendX(this.innerHTML);
             updateScreen(x);
         } else {
-            x = x + this.innerHTML;
+            appendX(this.innerHTML);
             updateScreen(x);
         }
     } else if (this.classList.contains('operator')) {
@@ -86,35 +131,52 @@ function executeButton(e) {
             operation = this.innerHTML;
         } else {
             updateScreen('ERR');
+            shake();
         }
     }
 }
 
-function updateScreen(value) {
-    const scrn = document.querySelector('.screen');
-    scrn.style.fontSize = '72px';
-    scrn.innerHTML = value;   
-    fontSize = parseFloat(window.getComputedStyle(scrn, null).fontSize);
-    while (scrn.scrollWidth > scrn.clientWidth) {
-        fontSize--;
-        scrn.style.fontSize = `${fontSize}px`;
-        console.log(window.getComputedStyle(scrn, null).fontSize);
-    }
- 
-}
-
-
-
 function pressButton(e) {
-    const pressedButton = document.querySelector(`button[data-key='${e.key}']`);
-    executedButton(pressedButton);
-
+    const pressedButton = document.querySelector(`button[data-key='${e.key}']`) 
+    if (pressedButton.innerHTML === 'AC') {
+        reset(true);
+    } else if (pressedButton.innerHTML === 'Enter') {
+        evaluate();        
+    } else if (pressedButton.classList.contains('number')) {
+        if ((!result) && operation === "") {
+            appendX(pressedButton.innerHTML);
+            updateScreen(x);
+        } else if (operation !== "") {
+            appendY(pressedButton.innerHTML);
+            updateScreen(y);
+        } else if (result === x) {
+            reset();
+            appendX(pressedButton.innerHTML);
+            updateScreen(x);
+        } else {
+            appendX(pressedButton.innerHTML);
+            updateScreen(x);
+        }
+    } else if (pressedButton.classList.contains('operator')) {
+        if (operation === "") {
+            operation = pressedButton.innerHTML;
+        } else if (operation !== "" && y !== "") {
+            evaluate(); // evaluates the answer when a second operation is pressed an y is non empty.
+            operation = pressedButton.innerHTML;
+        } else {
+            updateScreen('ERR');
+            shake();
+        }
+    }
 }
+
 
 const buttons = document.querySelectorAll('button');
-buttons.forEach((button) => button.addEventListener('click', executeButton));
+buttons.forEach((button) => button.addEventListener('click', clickButton));
 //buttons.forEach((button) => button.addEventListener('click', (e) => console.log(e)));
 
 window.addEventListener('keydown',pressButton);
+
+
 
 
